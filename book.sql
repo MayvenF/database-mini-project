6dd87eaed1aab1241b91535e58c23bdfe7aa2e7d
MayvenF <117817449+MayvenF@users.noreply.github.com>
2024-05-25 22:41:32 +0300

    Add files via upload

---
+++ book.sql
@@ -0,0 +1,24 @@
+INSERT INTO `Book` (`shelfID`,`authorID`,`publisherID`,`supplierID`,`ID`,`title`,`genre`,`condition`)
+VALUES
+  (13,2054,722207,171077,658435,"Nullam scelerisque neque sed sem","sem eget","aliquet"),
+  (39,2878,607620,200895,945469,"ac mi eleifend egestas. Sed pharetra,","ut, molestie","eu"),
+  (63,4954,196095,135027,345029,"amet, consectetuer adipiscing elit. Curabitur sed","Aliquam vulputate","Proin"),
+  (13,9577,987255,509466,135514,"luctus et","dapibus quam","dictum"),
+  (78,1063,254676,950440,350346,"ante lectus convallis est, vitae sodales nisi magna sed dui.","egestas blandit.","dui"),
+  (21,5742,385710,601901,520787,"urna suscipit nonummy.","pharetra, felis","laoreet,"),
+  (22,6902,813064,149370,816306,"sem ut dolor dapibus","pharetra.","eu"),
+  (84,4897,936161,749778,408207,"nunc ac mattis ornare, lectus ante dictum","pede.","adipiscing,"),
+  (89,4295,614766,161655,944948,"dolor, nonummy ac, feugiat","amet, consectetuer","in"),
+  (48,5871,996121,621430,159937,"cursus purus. Nullam scelerisque neque sed sem egestas","sapien,","sit");
+INSERT INTO `Book` (`shelfID`,`authorID`,`publisherID`,`supplierID`,`ID`,`title`,`genre`,`condition`)
+VALUES
+  (11,5599,263720,718724,136180,"aptent taciti sociosqu ad litora torquent per","Morbi quis","neque"),
+  (46,2353,489276,572146,527970,"eros nec tellus. Nunc lectus pede, ultrices","euismod","sagittis"),
+  (71,4808,824234,372448,654404,"ac risus. Morbi metus. Vivamus","sed dictum","ac"),
+  (3,1650,476351,985701,926661,"pellentesque massa lobortis","cursus","justo"),
+  (47,8772,485352,723214,361444,"hendrerit consectetuer, cursus et, magna. Praesent interdum ligula eu enim.","adipiscing elit.","vulputate"),
+  (2,9320,831366,520630,133835,"leo. Morbi","neque sed","quam."),
+  (46,3266,974618,456075,559916,"eget laoreet posuere, enim nisl elementum purus, accumsan","placerat, augue.","Aliquam"),
+  (24,3173,444588,143308,482504,"Cras lorem lorem, luctus ut,","sed","magna,"),
+  (62,9630,814130,598248,736146,"vitae","eu,","mattis."),
+  (8,9791,198041,269756,738540,"vulputate ullamcorper magna. Sed eu eros. Nam consequat","fames ac","id,");
