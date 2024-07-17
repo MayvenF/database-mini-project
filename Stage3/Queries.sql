eee90895897800447f7ff3bb377c67de5497b99a
jessicafeinmesser <117769110+jessicafeinmesser@users.noreply.github.com>
2024-07-17 20:35:18 +0300

    Update Queries.sql

--- Stage3/Queries.sql
+++ Stage3/Queries.sql
@@ -1,3 +1,59 @@
+--This query retrieves all books along with their corresponding authors and publishers.
+--It joins the book, writtenBy, author, publishedBy, publisher tables.
+
+SELECT 
+    book.title AS book_title,
+    author.name AS author_name,
+    publisher.name AS publisher_name
+FROM 
+    book
+JOIN 
+    writtenBy ON book.bookid = writtenBy.bookid
+JOIN 
+    author ON writtenBy.authorid = author.authorid
+JOIN 
+    publishedBy ON book.bookid = publishedBy.bookid
+JOIN 
+    publisher ON publishedBy.publisherid = publisher.publisherid;
+--Time: 1 second 202 msec
+
+
+--This query retrieves all reservations along with the names of the readers who made the reservations and the titles of the books reserved.
+  --It joins the reservation, reader, and book tables.
+
+SELECT 
+    reader.readername AS reader_name,
+    book.title AS book_title,
+    reservation.due_date AS due_date
+FROM 
+    reservation
+JOIN 
+    reader ON reservation.librarycard = reader.librarycard
+JOIN 
+    book ON reservation.bookid = book.bookid;
+--Time: 246 msec
+
+
+--This query updates the due date for all books reserved by a specific reader, identified by their readername. 
+--It joins the reservation table with the reader and book tables to ensure the correct books are updated.
+
+UPDATE 
+    reservation
+SET 
+    returndate = '2025-12-31'  -- Replace with the new due date
+FROM 
+    reader
+JOIN 
+    reservation AS r ON r.librarycard = reader.librarycard
+JOIN 
+    book ON r.bookid = book.bookid
+WHERE 
+    reservation.reservationid = r.reservationid
+    AND reader.readername = 'Lorraine Williams';  -- Replace with the specific reader's name
+
+--Time: 114 msec
+
+----------------------------------------
 --New queries after adding functions:
 ----------------------------------------
 -- Select query for View 1
